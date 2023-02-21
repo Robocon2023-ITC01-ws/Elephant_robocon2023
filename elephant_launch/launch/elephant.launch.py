7820fbad07310451f83502b3ce16e0e06d46015c
zeroboy0010 <zeroeverything001@gmail.com>
2023-02-21 14:40:47 +0700

    test_switch

--- elephant_launch/launch/elephant.launch.py
+++ elephant_launch/launch/elephant.launch.py
@@ -55,7 +55,7 @@ def generate_launch_description():
         reset_orientation_arg,
         frequency_arg,
         ros_imu_node,
-        # Node(package='elephant_localization', executable='localization'),
+        Node(package='elephant_localization', executable='localization'),
         Node(package='elephant_can',executable='can'),
         Node(package='elephant_can',executable='teleop'),
         Node(package='elephant_control',executable='test_controller'),
