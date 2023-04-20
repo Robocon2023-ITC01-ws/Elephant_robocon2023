b38be28aa5c56bd807ace65d83f866a0f7f6e67a
zeroboy0010 <zeroeverything001@gmail.com>
2023-04-20 12:06:51 +0700

    fixed on can node

--- elephant_can/elephant_can/can_node.py
+++ elephant_can/elephant_can/can_node.py
@@ -59,8 +59,9 @@ class can_class(Node):
                                             ####################
                         pub_msg.data = [(self.Tick[0]), (self.Tick[1]), (self.Tick[2])]         
                         self.publisher_.publish(pub_msg)
+                else :
+                    self.get_logger().error('time out on msg recv!!!')
             except can.CanOperationError :
-                print("time out on msg recv!!!")
                 pass
 
 def main(args=None):
